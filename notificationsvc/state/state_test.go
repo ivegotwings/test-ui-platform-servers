c46682f85a446662b11d4f921f40e851cbf656f3
shiv kumar <shiv@riversands-MacBook-Pro.local>
2020-05-09 23:15:54 +0530

    increment version test

--- notificationsvc/state/state_test.go
+++ notificationsvc/state/state_test.go
@@ -1,10 +1,34 @@
 package state
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/garyburd/redigo/redis"
+)
 
 func TestSTATE(t *testing.T) {
 	err := Connect(nil)
 	if err != nil {
 		t.Errorf("state failed to connect to %v", err)
 	}
+	conn.Send("SET", "TESTKEY", 1)
+	conn.Flush()
+	version, err := redis.Int(conn.Do("GET", "TESTKEY"))
+	if err == nil {
+		var newversion uint8
+		if version != 0 {
+			_version := uint8(version)
+			newversion = _version + 1
+		}
+		conn.Send("SET", "TESTKEY", newversion)
+		conn.Flush()
+	} else {
+		t.Errorf("state failed to update test key %v", err)
+	}
+
+	finalversion, err := redis.Int(conn.Do("GET", "TESTKEY"))
+	if finalversion != 2 {
+		t.Errorf("state failed to update test key %v", err)
+	}
+
 }
